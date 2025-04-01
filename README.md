5582f64287c00063aff72fad013da6a9d24b1c19
Sofia Velasquez Marin <s.velasquezm2@uniandes.edu.co>
2025-04-01 09:42:23 -0500

    Dejar main como plantilla

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MISW4407-Videojuegos
-Contenido de la clase Introducción al desarrollo de videojuegos (MISW4407)
+# PLANTILLA PROYECTO - SEMANA 01
+Esta plantilla contiene la estructura básica para comenzar los video-series de la semana 1 de "introducción al desarrollo de videojuegos con ECS"
--- Semana 1/Practica - Taller/src/ecs/components/c_enemy_spawner.py
+++
@@ -1,9 +0,0 @@
-
-
-class CEnemySpawner:
-    def __init__(self, spawn_events: list) -> None:
-        # Cada evento debe contener: time, enemy_type, position, triggered
-        self.spawn_events = [
-            {**event, "triggered": False} for event in spawn_events
-        ]
-        self.elapsed_time = 0.0
